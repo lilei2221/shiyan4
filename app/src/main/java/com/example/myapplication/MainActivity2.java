4b3439ae82096f019ea6c2b61500b68c100cf13e
lilei2221 <13101005840@163.com>
2024-11-03 15:48:08 +0800

    Initial commit

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -17,7 +17,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.shiyan3);
 
         // 数据源
         String[] animalNames = {"Lion", "Tiger", "Monkey", "Dog", "Cat", "Elephant"};
